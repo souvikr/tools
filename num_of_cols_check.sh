dba7db96c577cb21b3b36e4625977ccc0b057941
Souvik Roy <souvikroy5@gmail.com>
2024-06-25 11:35:05 +0530

    Update num_of_cols_check.sh

--- num_of_cols_check.sh
+++ num_of_cols_check.sh
@@ -1,40 +1,15 @@
 #!/bin/bash
 
 # Input file name
-INPUT_FILE="input.csv"
+INPUT_FILE="input.txt"
 
 # Output file for rows that do not have 52 columns
 OUTPUT_FILE="invalid_rows.txt"
 
-# Temporary directory for storing chunk results
-TEMP_DIR="temp_results"
-mkdir -p "$TEMP_DIR"
+# Clear the output file if it exists
+> "$OUTPUT_FILE"
 
-# Number of lines per chunk
-LINES_PER_CHUNK=1000000
-
-# Split the input file into smaller chunks
-split -l $LINES_PER_CHUNK "$INPUT_FILE" chunk_
-
-# Function to process a chunk
-process_chunk() {
-  local chunk_file=$1
-  local temp_output="${TEMP_DIR}/${chunk_file}.out"
-
-  awk -F'|' 'NF != 52 {print FNR}' "$chunk_file" > "$temp_output"
-}
-
-export -f process_chunk
-export TEMP_DIR
-
-# Use GNU Parallel to process chunks in parallel
-ls chunk_* | parallel process_chunk {}
-
-# Combine the results from all chunks into the final output file
-cat ${TEMP_DIR}/chunk_*.out > "$OUTPUT_FILE"
-
-# Clean up temporary files
-rm -r "$TEMP_DIR"
-rm chunk_*
+# Use awk to check each row
+awk -F'|' 'NF != 52 {print NR}' "$INPUT_FILE" > "$OUTPUT_FILE"
 
 echo "Check completed. Invalid rows (if any) are listed in $OUTPUT_FILE."
