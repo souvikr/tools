602788377047456d0fa93832d6737643d96fcc7f
Souvik Roy <souvikroy5@gmail.com>
2024-06-25 11:42:05 +0530

    Update num_of_cols_check.sh

--- num_of_cols_check.sh
+++ num_of_cols_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Input file name
-INPUT_FILE="input.txt"
+INPUT_FILE="input.csv"
 
 # Output file for rows that do not have 52 columns
 OUTPUT_FILE="invalid_rows.txt"
@@ -9,7 +9,13 @@ OUTPUT_FILE="invalid_rows.txt"
 # Clear the output file if it exists
 > "$OUTPUT_FILE"
 
-# Use awk to check each row
-awk -F'|' 'NF != 52 {print NR}' "$INPUT_FILE" > "$OUTPUT_FILE"
+# Use awk to check each row and include the number of columns
+awk -F'|' '
+{
+  if (NF != 52) {
+    print "Row " NR ": " NF " columns" > "'$OUTPUT_FILE'"
+  }
+}
+' "$INPUT_FILE"
 
 echo "Check completed. Invalid rows (if any) are listed in $OUTPUT_FILE."
